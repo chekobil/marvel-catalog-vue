440dbc689563493a52dc2d512c366b73d8260391
kobino <felipe.arg@gmail.com>
2024-09-24 01:15:24 +0200

    refactor(axios): Make https requests

--- src/plugins/axiosPlugin.ts
+++ src/plugins/axiosPlugin.ts
@@ -2,7 +2,7 @@ import { App } from "vue";
 import createAxiosInstance from '../services/axiosInstance'
 import { axiosKey } from './keys'
 
-const instance = createAxiosInstance('http://gateway.marvel.com', { apikey: import.meta.env.VITE_MARVEL_APIKEY })
+const instance = createAxiosInstance('https://gateway.marvel.com', { apikey: import.meta.env.VITE_MARVEL_APIKEY })
 
 export default {
     install: async (app: App) => {
