ced5ac2be39d848439d8dbce32f81994f87a54c8
kobino <felipe.arg@gmail.com>
2024-09-23 20:12:45 +0200

    new(ComicCard): Basic Card component

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="comic-card-container">
+      <img :src="thumbnailUrl" :alt="comic.title">
+      {{ comic.title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import {computed} from 'vue'
+const props = defineProps<{
+comic: Comic
+}>()
+const thumbnailUrl = computed( () => {
+  if (!props.comic) return ''
+  return `${props.comic.thumbnail.path}/portrait_medium.${props.comic.thumbnail.extension}`} )
+</script>
+
+<style lang="scss">
+.comic-card-container {
+  width: 12rem;
+  height: 100%;
+  border: 2px solid red;
+  border-radius: 0.8rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+  overflow: hidden;
+  img {
+      width: 100%;
+      height: auto;
+  }
+}
+</style>
