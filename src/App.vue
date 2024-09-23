876e495a5b00eafb9c0b2fbf5adf0f60b7ce47c2
kobino <felipe.arg@gmail.com>
2024-09-23 19:54:13 +0200

    refactor(App): useAxios to make a basic request

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import type { Ref } from 'vue'
+import useAxios from './services/useAxios'
+const $axios = useAxios()
+const catalog: Ref<Catalog> = ref({})
+  const isLoading: Ref<boolean> = ref(false)
+onMounted( async () => {
+  isLoading.value = true
+  catalog.value = await getCatalog()
+  isLoading.value = false
+})
+
+const getCatalog = async () => {
+  const url = "/v1/public/comics"
+  const res = await $axios(url, { params: { limit:12, orderBy: '-onsaleDate' } }) 
+  return res?.data?.data ?? {} 
+}
 </script>
 
 <template>
-  <div>
-    Basic
+  <div v-if="isLoading">
+    Loading ...
+  </div>
+  <div v-else-if="catalog.count === 0">
+    Catalog has no items
+  </div>
+  <div v-else>
+    Catalog has {{ catalog.count }} items
   </div>
 </template>
 
