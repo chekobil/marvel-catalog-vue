b09853e561b889782b789a0e98cfab6eec30a3db
kobino <felipe.arg@gmail.com>
2024-09-24 03:11:18 +0200

    refactor(router): Change history mode

--- src/plugins/routerDefinition.ts
+++ src/plugins/routerDefinition.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import CatalogView from '../components/CatalogView.vue'
 import ComicView from '../components/ComicView.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
