0ff204db2049afa88be230fda50e6bc37802b729
kobino <felipe.arg@gmail.com>
2024-09-24 03:18:15 +0200

    refactor(NavBar): NavBar as an independent component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue'
-import { useRouter } from 'vue-router'
 import ComicSearch from './components/ComicSearch.vue';
 import CatalogView from './components/CatalogView.vue';
+import NavBar from './components/NavBar.vue';
 type viewElementType = InstanceType<typeof CatalogView>
 type searchElementType = InstanceType<typeof ComicSearch>
 const viewElement = useTemplateRef<viewElementType>('viewElement')
 const searchElement = useTemplateRef<searchElementType>('searchElement')
-const router = useRouter()
-const handleGoTo = (path: string) => {
-  router.push({path, replace: true})
-}
 
 const handleChangeSearch = ({text, character}: CharacterEmitData) => {
   if (viewElement.value && typeof viewElement.value.getFilteredCatalog === 'function') {
     viewElement.value.getFilteredCatalog(text ?? '', character ?? {})
   }
 }
-
 const handleClearSearch = () => {
   if (searchElement.value && typeof searchElement.value.cleanSearch === 'function')
   searchElement.value.cleanSearch()
@@ -26,7 +21,7 @@ const handleClearSearch = () => {
 
 <template>
   <main>
-    <button @click="handleGoTo('/')">inicio</button>
+    <NavBar />
     <ComicSearch ref="searchElement" @change="handleChangeSearch"/>
     <RouterView v-slot="{ Component }">
       <component ref="viewElement" :is="Component" @clear-search="handleClearSearch" />
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button @click="handleGoTo('/')">inicio</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const handleGoTo = (path: string) => {
+  router.push({path, replace: true})
+}
+</script>
+
+<style scoped>
+
+</style>
