d7bb7ac92a2cc5f11e0c31647d41ff05b2dd2a1f
kobino <felipe.arg@gmail.com>
2024-09-24 13:43:30 +0200

    chore(pinia): Added Pinia to manage stores

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import router from './plugins/routerDefinition'
 import axiosPlugin from './plugins/axiosPlugin'
+const pinia = createPinia()
 
 import './style.css'
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import '@shoelace-style/shoelace'
 
 const app = createApp(App)
+app.use(pinia)
 app.use(router)
 app.use(axiosPlugin)
 app.mount('#app')
