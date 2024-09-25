fcd2f2faa4992d27a3275397e70739c2c795e2c1
kobino <felipe.arg@gmail.com>
2024-09-25 15:03:33 +0200

    refactor(build): Added postbuild script

---
+++ scripts/postbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SOURCE="dist/index.html"
+DESTINATION="dist/404.html" # for vercel
+DESTINATION2="dist/200.html" # for surge.sh
+
+if [ -f "$SOURCE" ]; then
+    cp "$SOURCE" "$DESTINATION"
+    cp "$SOURCE" "$DESTINATION2"
+fi
