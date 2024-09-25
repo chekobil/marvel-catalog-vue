9670f6c775f99de3d63f0276ec835f850ea03c94
chekobil <felipe.arg@gmail.com>
2024-09-25 14:27:27 +0200

    refactor(404): Added a not found view

---
+++ src/components/NotFoundView.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>Not found</h2>
+    <sl-button @click="handleGoTo('/')">Go to homepage</sl-button>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const handleGoTo = (path: string) => {
+  router.push({path, replace: true})
+}
+</script>
--- src/plugins/routerDefinition.ts
+++ src/plugins/routerDefinition.ts
@@ -2,11 +2,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import CatalogView from '../components/CatalogView.vue'
 import ComicView from '../components/ComicView.vue'
+import NotFoundView from '../components/NotFoundView.vue'
 
 const routes = [
   { path: '/', component: CatalogView },
   { path: '/comic/:id', component: ComicView },
-  { path: '/:pathMatch(.*)*', component: CatalogView },
+  { path: '/:pathMatch(.*)*', component: NotFoundView },
 ]
 
 const router = createRouter({
